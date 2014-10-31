510e83b61070c3cdb42bbc9362968059d8b15f49
LeperGnome <bancu.cristi@gmail.com>
2014-10-31 19:12:04 +0200

    add google+ auth capabilities

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
+
 var Hapi = require('hapi');
 var Good = require('good');
 var WcfRepoAdapter = require('./lib/wcfServRepo');
+ 
+var server = new Hapi.Server('localhost', 27321);
 
-var server = Hapi.createServer('localhost', process.env.PORT || 27321, {
-    cors: true
-});
-
-
-server.route({
-    method: 'GET',
-    path: '/',	
-    handler: function (request, reply) {
-        reply('I am alive');
-    }
+server.views( {
+	engines: {
+            html: require('handlebars')
+        },
+        path: __dirname + '/lib/login/templates',
+        partialsPath: __dirname + '/lib/login/templates',
+        layout: true
 });
 
 server.pack.register([
@@ -24,10 +23,67 @@ server.pack.register([
         options: {
             collection : 'wcfservice'
         }
-    }],
-	function() {
-		server.start(function () {
+    },
+	{ plugin: require('lout') },
+    { plugin: require('bell') },
+    { plugin: require('hapi-auth-cookie')},
+    { plugin: require('./lib/login/plugins/auth')}
+  ], 
+	function(err) { 
+		if(err) throw err;
+		
+		server.route([
+	{	
+		path: '/myprofile',
+		method: 'GET',
+		config: {
+			auth: 'session',
+			handler: function(request, reply) {
+				reply('<html><head><title>Login page</title></head><body><h3>Welcome '
+				  + JSON.stringify(request.auth.credentials, null, 4)
+				  + '!</h3><br/><form method="get" action="/logout">'
+				  + '<input type="submit" value="Logout">'
+				  + '</form></body></html>');
+			}
+		}
+	}, 
+	{
+		path: '/',
+		method: 'GET',
+		config: {  // try with redirectTo disabled makes isAuthenticated usefully available
+			auth: {
+				strategy: 'session',
+				mode: 'try'
+			},
+			plugins: { 'hapi-auth-cookie': { redirectTo: false } }
+		},
+		handler: function(request, reply) {
+			reply.view('index', {
+				auth: JSON.stringify(request.auth),
+				session: JSON.stringify(request.session),
+				isLoggedIn: request.auth.isAuthenticated
+			});
+		}
+	}, {
+		path: '/{path*}',
+		method: 'GET',
+		handler: {
+			directory: {
+				path: './lib/login/public',
+				listing: false,
+				index: true
+			}
+		}
+	}
+]);
+		
+		server.start(function() {
 			console.log('Server started at: ' + server.info.uri);
-		})
-});
+		});
+	}
+);
+	
+	
+
+
 
