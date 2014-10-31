ad31727001ac0b957fdefd504ea025b389e1b7e3
unknown <cristianba@BUCH-CRISTIANBA.Sparkware.corp>
2014-10-31 14:21:35 +0200

    1st draft

---
+++ app.js
@@ -0,0 +1,33 @@
+var Hapi = require('hapi');
+var Good = require('good');
+var WcfRepoAdapter = require('./lib/wcfServRepo');
+
+var server = Hapi.createServer('localhost', process.env.PORT || 27321, {
+    cors: true
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/',	
+    handler: function (request, reply) {
+        reply('I am alive');
+    }
+});
+
+server.pack.register([
+    { 
+		plugin: Good 
+	},
+    {
+        plugin: WcfRepoAdapter,
+        options: {
+            collection : 'wcfservice'
+        }
+    }],
+	function() {
+		server.start(function () {
+			console.log('Server started at: ' + server.info.uri);
+		})
+});
+
