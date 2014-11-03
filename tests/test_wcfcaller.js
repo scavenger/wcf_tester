707054ca9cd6e46eabca15c9ea242e802d0002a9
scavenger <cgcmsu@yahoo.com>
2014-11-03 19:17:23 +0200

    wcfCaller fixes + updated test to see errorcode

--- tests/test_wcfcaller.js
+++ tests/test_wcfcaller.js
@@ -4,13 +4,29 @@ var caller = require('../lib/wcfcaller');
 var url = 'http://buch-cristianmi.sparkware.corp/ODS/Services/DictionariesService.svc?wsdl';
 
 soap.createClient(url, function(err, client){
-	if(!err){
+	if(client){
 		var callList = caller.buildCallList(client);
+		var noOfMethods = callList.length;
+		var finished = 0;
+
 		callList.forEach(function(el, idx){
 
 			client[el.method](el.input, function(err, result) {
-				console.log(el.method + ' => ' + err);
+				
+				finished++;
+
+				console.log(el.method + ' => ' +  JSON.stringify(result[el.output].ErrorCode));
+				allIsDone(noOfMethods, finished);
 			});
+
 		});
+	}else{
+		console.log('Incorrect WSDL url!');
 	}
 });
+
+function allIsDone(total, current){
+	if(total == current){
+		console.log('>> ALL is DONE!')
+	}
+}
